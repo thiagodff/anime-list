33f579ae81d4a4518b953dd27cb6cf305aba0743
Thiago Fernandes Dornelles <thiago.fdornelles@gmail.com>
2021-04-25 20:02:55 -0300

    feat: sidebar queries hook

--- src/hooks/usePaginatedCategoriesQuery/index.ts
+++ src/hooks/usePaginatedCategoriesQuery/index.ts
@@ -3,10 +3,10 @@ import { QueryFunctionContext, useQuery } from 'react-query';
 import { TopAiringResponse, ApiError, TopAiringAnime } from '@common/types/api';
 import { api } from '@services';
 
-export const fetchAnime = async (ctx: QueryFunctionContext<string[]>) => {
+export const fetchAnime = async (ctx: QueryFunctionContext<Array<string | number>>) => {
   const [key, page] = ctx.queryKey;
 
-  const [category, subCategory] = key.split('-');
+  const [category, subCategory] = String(key).split('-');
 
   const { data } = await api.get<TopAiringResponse>(
     `/${category}/anime/${page}/${subCategory}`
@@ -20,9 +20,9 @@ export const usePaginatedCategoriesQuery = (
   subCategory: string,
   page: number,
 ) =>
-  useQuery<TopAiringAnime[], ApiError, TopAiringAnime[], string[]>(
+  useQuery<TopAiringAnime[], ApiError, TopAiringAnime[], Array<string | number>>(
     // habitualmente utiliza-se apenas duas posicoes do array
-    [`${category}-${subCategory}`, String(page)],
+    [`${category}-${subCategory}`, page],
     fetchAnime,
     {
       keepPreviousData: true,
